3c9f189b8fcc775ba0286c418b77bbd66ab156f8
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-03-28 10:32:33 +0530

    Add basics of oop

---
+++ OOP/src/Basics/Main.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Madusanka",25,"Ananda college");
+        System.out.println(student1.toString());
+    }
+}
