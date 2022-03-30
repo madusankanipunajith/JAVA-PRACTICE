c26caf20b14f6be14a9279db60f896ab0c175760
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2022-03-30 10:21:40 +0530

    Add new changes to the OOP

--- OOP/src/Interface/EX01/Bicycle.java
+++ OOP/src/Interface/EX01/Bicycle.java
@@ -1,6 +1,19 @@
 package Interface.EX01;
 
 public interface Bicycle {
+    int x = 0; // public static final by default
+
+    // public abstract by default
     void applyBreak(int decrement);
     void speedUp(int increment);
+
+    // default method after 1.8 version
+    default void printX(){
+        System.out.println("X : "+this.x);
+    }
+
+    // static method after 1.8 version
+    static void statMethod(){
+        System.out.println("Hello static method");
+    }
 }
--- OOP/src/Interface/EX01/Demo.java
+++ OOP/src/Interface/EX01/Demo.java
@@ -3,6 +3,9 @@ package Interface.EX01;
 public class Demo {
     public static void main(String[] args) {
         Bicycle bicycle = new MountainBike(20,10,1);
+        System.out.println(Bicycle.x);
+        bicycle.printX();
+
         MountainBike b = (MountainBike) bicycle;
 
         bicycle.applyBreak(1);
